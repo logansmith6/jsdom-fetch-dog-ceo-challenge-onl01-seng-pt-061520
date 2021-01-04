71d3f0004ca974bcfcbb6de51a16100a99c0cb92
Logan Smith <logsmith06@gmail.com>
2021-01-04 19:50:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -38,8 +38,9 @@ function fetchImages(){
 
           li[i].addEventListener('click', function(){
             target.classList.add("clr-blue")
-            }
           })
+          }
+        }
 
   }
 
