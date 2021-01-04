296feb74748237c6139cb031c99c14645141ca8e
Logan Smith <logsmith06@gmail.com>
2021-01-04 18:50:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ console.log('%c HI', 'color: firebrick')
 function fetchImages(){
   return fetch('https://dog.ceo/api/breeds/image/random/4')
     .then(response => response.json())
-    .then(json => addImages(json));
+    .then(json => renderImages(json));
 }
 
 
