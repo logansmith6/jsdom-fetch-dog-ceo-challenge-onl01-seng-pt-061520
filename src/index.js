51989c7995bc804c24fe4ea61cf4950fd03af0d6
Logan Smith <logsmith06@gmail.com>
2021-01-04 20:55:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -49,8 +49,8 @@ function fetchImages(){
 
 
   function listenForSort(){
-      const selectLi = document.getElementById("breed-dropdown");
-        selectLi.addEventListener("change", userSelection);
+      const li = document.getElementById("breed-dropdown");
+        li.addEventListener("change", userSelection);
   }
 
   function userSelection(x){
@@ -61,16 +61,16 @@ function fetchImages(){
   function sortBreeds(alpha){
     const breedList = document.getElementById("dog-breeds");
     const newList = breedList.cloneNode(false);
-       filterList = Array.from(breedList.childNodes);
+    filterList = Array.from(breedList.childNodes);
 
-    for (const selectLi of filterList) {
-      if (selectLi.textContent[0] != alpha) {
-          selectLi.classList.add("sorted");
+    for (const li of filterList) {
+      if (li.textContent[0] != alpha) {
+          li.classList.add("sorted");
         } else {
-          selectLi.classList.remove("sorted");
+          li.classList.remove("sorted");
         }
 
-    newList.appendChild(selectLi);
+    newList.appendChild(li);
     }
     breedList.parentNode.replaceChild(newList, breedList);
   }
