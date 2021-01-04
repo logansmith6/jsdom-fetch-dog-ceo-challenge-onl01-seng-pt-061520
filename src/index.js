31c6f0aafc1c4e923a47ab5f41a0faa895c689e7
Logan Smith <logsmith06@gmail.com>
2021-01-04 20:37:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function fetchImages(){
   }
 
   function sortBreeds(alpha){
-    const breedList = document.querySelectorAll("dog-breeds > p");
+    const breedList = document.querySelectorAll(".dog-breed > p");
 
 
     const sortedList = breedList.filter(breed => breed.startsWith(alpha));
