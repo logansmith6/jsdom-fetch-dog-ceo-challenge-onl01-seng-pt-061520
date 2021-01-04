eea8d60d578a954d22e7efb341e8ab03ffde31aa
Logan Smith <logsmith06@gmail.com>
2021-01-04 18:05:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 console.log('%c HI', 'color: firebrick')
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+let image = name.map((picName) => {
+  return picName
+})
+
+fetch(imgUrl + image)
+  .then(response => response.json())
+  .then(images =>console.log(fetchUrl + images));
