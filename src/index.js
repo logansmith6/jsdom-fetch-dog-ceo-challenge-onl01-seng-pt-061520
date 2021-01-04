e74ae0044cdb453318ab796b8c12a1a337759c8b
Logan Smith <logsmith06@gmail.com>
2021-01-04 20:16:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function fetchImages(){
         for (i = 0; i < li.length; i++){
 
           li[i].addEventListener("click", function(){
-            li[i].style.coor = "purple";
+            document.body.style.color = "purple";
           })
         }
   }
