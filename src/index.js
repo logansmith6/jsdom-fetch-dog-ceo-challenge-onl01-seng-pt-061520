7b18c46839058de39c8d5fdd7a50bd8e6b8300fd
Logan Smith <logsmith06@gmail.com>
2021-01-04 18:15:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-let image = name.map((picName) => {
-  return picName
-})
 
-fetch(imgUrl + image)
-  .then(response => response.json())
-  .then(images =>console.log(fetchUrl + images));
+function fetchImages(){
+  return fetch(imgUrl + image)
+    .then(response => response.json())
+    .then(images =>console.log(fetchUrl + images));
+}
+
+
+
+
+  function addImages(){
+    const div = document.getElementById('dog-image-container');
+    json.forEach(image => {
+      const h2 = document.createElement('img')
+        h2.innerHTML = image.src
+        div.appendChild(img)
+    })
+  }
